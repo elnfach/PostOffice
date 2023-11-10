bae9e0310291a009030f646434ab8c30ebb5842b
elnfach <vpetuhov160@gmail.com>
2023-11-10 21:49:59 +0800

    test

--- PostOffice/Form1.cs
+++ PostOffice/Form1.cs
@@ -10,29 +10,37 @@ using System.Windows.Forms;
 
 using Dadata;
 using Dadata.Model;
+using PostOffice.src;
 
 namespace PostOffice
 {
     public partial class Form1 : Form
     {
+        AddressWrap addressWrap;
+        string token = "edb182bc7bb5f773b81de1ee9098c6a9b74fa7cd";
         public Form1()
         {
             InitializeComponent();
-            Method();
         }
 
-        async void Method()
+        async void Method(double lat, double lon, int radius)
         {
-            var token = "edb182bc7bb5f773b81de1ee9098c6a9b74fa7cd";
-            var api = new SuggestClientAsync(token);
-            var response = await api.Iplocate("46.226.227.20");
-            var address = response.location.data.country;
-            Console.WriteLine(address);
+            var api = new OutwardClientAsync(token);
+            var response = await api.Geolocate<PostalUnit>(lat, lon, radius);
+            addressWrap = new AddressWrap(response.suggestions);
+            richTextBox1.Text = addressWrap.GetData();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 51.816468660013605, 107.65143629319506
+            Method(51.816468660013605, 107.65143629319506, 1000);
+            //Method(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToInt32(textBox3.Text));
         }
     }
 }
---
+++ PostOffice/src/AddressWrap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOffice.src
+{
+    internal class AddressWrap : Dadata.Model.Address
+    {
+        string data;
+        public AddressWrap(IList<Dadata.Model.Suggestion<Dadata.Model.PostalUnit>> suggestions) 
+        {
+            int counter = 0;
+            data = "{\n" +
+                "\"suggestions\": [\n";
+                
+            foreach (var item in suggestions)
+            {
+                counter++;
+                data += "\t{\n";
+                data += $"\t\"value\": {item.value},\n";
+                data += $"\t\"unrestricted_value\": {item.unrestricted_value},\n";
+                data += "\t\"data\": {\n";
+                data += $"\t\t\"postal_code\":{item.data.postal_code},\n";
+                data += $"\t\t\"is_closed\":{item.data.is_closed},\n";
+                data += $"\t\t\"type_code\":{item.data.type_code} ,\n";
+                data += $"\t\t\"address_str\":{item.data.address_str} ,\n";
+                data += $"\t\t\"address_kladr_id\":{item.data.address_kladr_id} ,\n";
+                data += $"\t\t\"address_qc\":{item.data.address_qc}  ,\n";
+                data += $"\t\t\"geo_lat\":{item.data.geo_lat}   ,\n";
+                data += $"\t\t\"geo_lon\":{item.data.geo_lon}  ,\n";
+                data += $"\t\t\"schedule_mon\":{item.data.schedule_mon}  ,\n";
+                data += $"\t\t\"schedule_tue\":{item.data.schedule_tue}  ,\n";
+                data += $"\t\t\"schedule_wed\":{item.data.schedule_wed}  ,\n";
+                data += $"\t\t\"schedule_thu\":{item.data.schedule_thu}  ,\n";
+                data += $"\t\t\"schedule_fri\":{item.data.schedule_fri}  ,\n";
+                data += $"\t\t\"schedule_sat\":{item.data.schedule_sat}  ,\n";
+                data += $"\t\t\"schedule_sun\":{item.data.schedule_sun}   \n";
+                data += "\t\t}\n";
+                if (suggestions.Count > counter)
+                {
+                    data += "\t},\n";
+                }
+                else
+                {
+                    data += "\t}\n";
+                }
+            }
+
+            data += "]\n";
+            data += "}\n";
+        }
+
+        public string GetData()
+        {
+            return data;
+        }
+    }
+}
